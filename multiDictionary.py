873f149c2f0d7e7f7bf9cf19e3a341e5cee3c9b0
lorenzo maccarrone <lorenzo.maccarrone31@gmail.com>
2024-03-20 16:51:47 +0100

    aggiunto commento per provare il sistemi di commit e push in quanto stava dando dei problemi

--- multiDictionary.py
+++ multiDictionary.py
@@ -1,7 +1,10 @@
 import dictionary as d
 import richWord as rw
 
-
+"""
+Questa classe ha il compito di gestire l'accesso ai vari dizionari
+salvati come elementi di tipo Dictionary
+"""
 class MultiDictionary:
 
 
