0d942139ca44beee53fba4406a1d8beaecfb9aed
lorenzo maccarrone <lorenzo.maccarrone31@gmail.com>
2024-03-20 16:47:40 +0100

    tolto errore grammaticale nel commento dovuto alla stanchezza

--- multiDictionary.py
+++ multiDictionary.py
@@ -2,7 +2,7 @@ import dictionary as d
 import richWord as rw
 
 """
-Questa classe ha il compito di gestire l'accesso hai vari dizionari
+Questa classe ha il compito di gestire l'accesso ai vari dizionari
 salvati come elementi di tipo Dictionary
 """
 class MultiDictionary:
