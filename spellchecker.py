9484e99d7f6040b8d4ef32ffc0e0a262e444d180
lorenzo maccarrone <lorenzo.maccarrone31@gmail.com>
2024-03-20 16:42:25 +0100

    aggiunto il timer e aggiunta funzione per eliminare spazi nell'input

--- spellchecker.py
+++ spellchecker.py
@@ -8,9 +8,11 @@ class SpellChecker:
         pass
 
     def handleSentence(self, txtIn, language):
+        start_time = time.perf_counter()
         txtInput=replaceChars(txtIn.lower())
         #dapprima salviamoci le parole in un vettore in modo da poterle controllare una ad una
         input=txtInput.split(" ")
+        input=eliminaSpazi(input)
         finalInput=[] #lista di richWord (con la parola in input e se è giuista o meno)
         finalInput.clear()
         if language=="italian":
@@ -27,9 +29,14 @@ class SpellChecker:
         a questo punto abbiamo la lista finalInput con le parole e se sono state trovate o meno
         quindi possiamo stampare a video il risultato
         """
+        end_time=time.perf_counter()
+        elapsed_time=end_time-start_time
+        print("------------------------------------\n"+
+              "    Le parole sbagliate sono   ")
         for word in finalInput:
             if not word.corretta:
                 print(word)
+        print(f"Tempo di ricerca con contains:{elapsed_time}")
 
 
     def printMenu(self):
@@ -50,3 +57,11 @@ def replaceChars(text):
         # scambia il carattere in questione c con un ""
         text = text.replace(c, "")
     return text
+def eliminaSpazi(input):
+    #restituisce un vettore di str che non contenga spazi
+    inputCorretto=[]
+    for i in input:
+        if i!="":
+            inputCorretto.append(i)
+    return inputCorretto
+
